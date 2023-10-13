d7f30807a88eb25fbc1555db9d5b35deebbf1106
ALTHAF SHAJAHAN <118408773+ALTHAFalthaf@users.noreply.github.com>
2023-10-13 16:18:15 +0530

    Add files via upload
    updated

--- urls.py
+++ urls.py
@@ -1,23 +1,8 @@
+
 from django.contrib import admin
-from django.urls import path
-#from django.contrib.auth import views as auth_views
-from .import views
-from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
+from django.urls import path, include
 
 urlpatterns = [
-   path('',views.index,name='index'),
-   path('signup', views.signup, name='signup'),
-   path('login', views.loginn, name='login'),
-   path('homepage', views.homepage, name='homepage'),
-   path('logout', views.user_logout, name='logout'),
-   path('adminreg',views.adminreg, name='adminreg'),
-
-
-
-
-   path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
-   path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-   path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-   path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('admin/', admin.site.urls),
+    path('', include('vaccine.urls')),
 ]
